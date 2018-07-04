4ab6f8ccb6b23b6bc3a2f0909277c14f871a0172
Jakub Olejník <olejnjak@gmail.com>
2018-07-04 14:58:32 +0200

    Prepare example app

---
+++ SimpleImagePickerExample/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  SimpleImagePickerExample
+//
+//  Created by Jakub Olejník on 04/07/2018.
+//
+
+import UIKit
+
+@UIApplicationMain
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
+        return true
+    }
+}
+
---
+++ SimpleImagePickerExample/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  SimpleImagePickerExample
+//
+//  Created by Jakub Olejník on 04/07/2018.
+//
+
+import UIKit
+import SnapKit
+import SimpleImagePicker
+
+final class ViewController: UIViewController {
+
+    private weak var imageView: UIImageView!
+    private weak var getPhotoButton: UIButton!
+    
+    private lazy var imagePicker: ImagePicker = { [weak self] in
+        let permissionAlert = PermissionAlertConfiguration(title: "Need permission", message: "We need more permission", settings: "Go to settings")
+        return ImagePicker(cancelTitle: "Cancel", permissionConfig: permissionAlert) { [weak self] image in
+            self?.imageView.image = image
+        }
+    }()
+    
+    // MARK: View life cycle
+    
+    override func loadView() {
+        super.loadView()
+        
+        view.backgroundColor = .white
+        
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = UIColor.lightGray.withAlphaComponent(0.1)
+        view.addSubview(imageView)
+        imageView.snp.makeConstraints { (make) in
+            make.leading.trailing.top.equalToSuperview()
+            make.height.equalToSuperview().multipliedBy(0.75)
+        }
+        self.imageView = imageView
+        
+        let getPhotoButton = UIButton(type: .system)
+        getPhotoButton.setTitle("Get photo", for: .normal)
+        view.addSubview(getPhotoButton)
+        getPhotoButton.snp.makeConstraints { (make) in
+            make.top.equalTo(imageView.snp.bottom).offset(10)
+            make.centerX.equalToSuperview()
+            make.width.lessThanOrEqualToSuperview().offset(-30)
+        }
+        self.getPhotoButton = getPhotoButton
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        getPhotoButton.addTarget(self, action: #selector(getPhotoButtonTapped), for: .primaryActionTriggered)
+    }
+    
+    // MARK: UI actions
+    
+    @objc
+    private func getPhotoButtonTapped() {
+        let sources = [ImagePickerSource(source: .camera, title: "Camera"), ImagePickerSource(source: .photoLibrary, title: "Photo library")]
+        
+        imagePicker.presentImagePicker(withSources: sources, mediaTypes: [ImagePickerMediaType.image], from: self)
+    }
+}
+
